12d983cd348f90274c55a39e1f396a62ff12672a
Helbob <millehelbo@outlook.dk>
2023-09-07 18:49:15 +0200

    ssss

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 "use strict";
 
 import { apiKeyOpenWeather } from "./info.js";
+
+const urlOpenWeather = "https://api.openweathermap.org/";
+
+const queryOpenWeather = (query) => {
+  const endpointOpenWeather = `${urlOpenWeather}data/2.5/weather?q=${query}&appid=${apiKeyOpenWeather}`;
+  console.log(endpointOpenWeather);
+
+  fetch(endpointOpenWeather, {
+    headers: {
+      Authorization: `Bearer ${apiKeyOpenWeather}`,
+    },
+  })
+    .then((response) => response.text())
+    .then((data) => showFilms(JSON.parse(data)))
+    .catch((error) => console.log("error", error));
+};
